f6197e9e29f892ca731dc66aa827290340b4d5c9
ist188123 <paulorosario@sapo.pt>
2019-06-22 11:33:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,57 +29,57 @@ client.on("message", async (msg) => {
   let adicional = "";
 
 
-  
-  
-
-if (msg.channel.name.startsWith('_raid')) {
-  
 
 
-  
 
-if (msg.content.startsWith('!h') && msg.content.length > 2) {
- 
-  msg.channel.send({
-    embed: {
-      color: 3447003,
-      description: "---msg.channel.name---:" + msg.channel.name
+  if (msg.channel.name.startsWith('_raid')) {
+
+
+
+
+
+    if (msg.content.startsWith('!h') && msg.content.length > 2) {
+
+      msg.channel.send({
+        embed: {
+          color: 3447003,
+          description: "---msg.channel.name---:" + msg.channel.name
+        }
+      });
+
+      var nomecanal = msg.channel.name
+      var thoras = nomecanal.split("-");
+      var horas = thoras[thoras.length - 3];
+
+
+
+      var novahora = msg.content.substring(2);
+
+      // console.log(horas)
+      var novotitulo = nomecanal.replace(horas, novahora)
+      msg.channel.send({
+        embed: {
+          color: 3447003,
+          description: "---novotitulo---:" + novotitulo
+        }
+      });
+      //onsole.log(novotitulo)
+
+      msg.guild.channels.find("name", nomecanal).setName(novotitulo);
+
+
+
     }
-  });
-
-   var nomecanal= msg.channel.name
-   var thoras = nomecanal.split("-");
-    var horas = thoras[thoras.length - 3];
-  
-  
-   
-             var novahora = msg.content.substring(2);
-  
-   // console.log(horas)
-   var novotitulo=nomecanal.replace(horas,novahora)
-   msg.channel.send({
-    embed: {
-      color: 3447003,
-      description: "---novotitulo---:" + novotitulo
-    }
-  });
-   //onsole.log(novotitulo)
-
-msg.guild.channels.find("name",nomecanal ).setName(novotitulo);
-  
-  
-  
-}
-//--fim novo --
- //--------------------------------------     
+    //--fim novo --
+    //--------------------------------------     
 
 
 
 
-        
+
   }
 
-   }
+
 
 
 
