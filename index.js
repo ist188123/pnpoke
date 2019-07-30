5fa2e67f2d125ca324da9665e43c0ad752189c1c
ist188123 <paulorosario@sapo.pt>
2019-07-30 11:48:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = "+";
 const http = require('http');
 
 
@@ -18,66 +17,15 @@ const http = require('http');
 
 
 client.on("message", async (msg) => {
-  let mewtwo = "";
-  let cor = "0x00AE86";
-  let mais = "";
-  let autor = ""
-  let quantidade = 0;
-  let obs = "";
-  var regra = "Mod"
-  let pkmraid = "Desconhecido";
-  let adicional = "";
+  
 
 
-
-
-
-  if (msg.channel.name.startsWith('_raid')) {
-
-
-
-
-
-    if (msg.content.startsWith('!h') && msg.content.length > 2) {
-
+       
+          var Attachment = (message.attachments).array();
+          msg.channel.send(Attachment); //outputs array
+          msg.channel.send(Attachment[0].url);
+         
      
-      var nomecanal = msg.channel.name
-      var thoras = nomecanal.split("-");
-      var horas = thoras[thoras.length - 3];
-
-
-
-      var novahora = msg.content.substring(2);
-
-      // console.log(horas)
-      var novotitulo = nomecanal.replace(horas, novahora)
-      msg.channel.send({
-        embed: {
-          color: 3447003,
-          description: "Nova hora Raid:" + novahora +" alterado por : "+msg.author
-        }
-      });
-      //onsole.log(novotitulo)
-
-     msg.guild.channels.find("name", nomecanal).setName(novotitulo);
-     msg.channel.send('👍')
-
-
-    }
-    //--fim novo --
-    //--------------------------------------     
-
-
-
-
-
-  }
-
-
-
-
-
-
 
 
   //--- fim teste ----
