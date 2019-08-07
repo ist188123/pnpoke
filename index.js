b4831299000a547a2b79812dbceb96a484b2962c
ist188123 <paulorosario@sapo.pt>
2019-08-07 07:16:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const http = require('https');
-var tesseract = require('node-tesseract');
+
 
 //console.log(tamanhoFicheiro)
 
