6ed3ededfded4fbcd343eba6c2d4378e5d4c5fe5
ist188123 <paulorosario@sapo.pt>
2019-08-24 00:56:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -190,20 +190,48 @@ const filter = (reaction, user) => {
 };
 	 
 	 
-	 
+var ovo='https://torneiopokemongopinhalnovo.webnode.pt/_files/200000047-74c3d74c3f/450/question.jpg';
+
+switch (tier) {
+    case "1":
+        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
+        break;
+    case "2":
+        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
+        break;
+
+    case "3":
+        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
+
+        break;
+    case "4":
+        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
+
+        break;
+    case "5":
+        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png";
+
+        break;
+
+}	 
 	 
 	 
 //------------
-/**	 
+	 
 msg.channel.send({embed: {
     color: 15158332,
+	
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
     title: "ANUNCIO RAID",
     url: "http://google.com",
+		
     description: "\@everyone",
+	thumbnail: {
+		url: ovo,
+	},
     fields: [{
         name: "Nivel ",
         value: tier
@@ -243,55 +271,7 @@ msg.channel.send({embed: {
       
     }
   }
-})
-	
-*/	
-	 
-	 
-var ovo='https://torneiopokemongopinhalnovo.webnode.pt/_files/200000047-74c3d74c3f/450/question.jpg';
-
-switch (tier) {
-    case "1":
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
-        break;
-    case "2":
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
-        break;
-
-    case "3":
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
-
-        break;
-    case "4":
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
-
-        break;
-    case "5":
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png";
-
-        break;
-
-}
-	 
-	const raidinfomsg = new Discord.RichEmbed()
-                            .setColor('#FF0000')
-                            .setTitle("\@everyone")
-                            .setURL('https://discord.js.org/')
-                            .setAuthor('ANUNCIO RAID ' + tier, ovo, 'https://discord.js.org')
-                            .setThumbnail(pokemon)
-                            .addField('Ginásio', gymraid)
-                            //.addBlankField()
-                            .addField('Abre', "**" + hora_inicio_raid + "**", true)
-                            .addField('Termina', "**" + hora_fim_raid + "**", true)
-                            .setTimestamp()
-                            .setFooter('Anunciado por : ' + msg.author.username, 'https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png');
-                        msg.guild.channels.find("name", "info-raids").sendMessage(raidinfomsg) 
-	 
-	 
-	 
-	 
-	
-	.then(msg => {
+}).then(msg => {
 	
 	 if(pokemon=='?'){
 		  msg.react('\u0031\u20E3')
